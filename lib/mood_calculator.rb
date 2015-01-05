097a5878b99bf47a19a66af366a6d14723507499
Fabricio Buzeto <nuk.anime13@gmail.com>
2015-01-05 11:20:45 -0200

    Mood seems to be calculated properly

--- lib/mood_calculator.rb
+++ lib/mood_calculator.rb
@@ -1,23 +1,51 @@
 class MoodCalculator
   def initialize user
-    @grouping = []
+    @grouping = {}
+    @commit_index = 0
+    @track_index = 0
   end
 
   def run track_list, commit_list
-    track_list.each do |track|
-      commit_list.each do |commit|
-        handle_if_match commit, track
-      end
+    while has_tracks track_list
+      track = track_list[@track_index]
+      process_track track, commit_list
     end
-    @grouping
+    @grouping.values.sort_by {|c| -c[:count]}
+  end
+
+  def has_tracks track_list
+    @track_index < track_list.size
+  end
+
+  def process_track track, commit_list
+    if has_commits commit_list and
+        not handle_if_match commit_list[@commit_index], track
+      @commit_index += 1
+    else
+      @track_index += 1
+    end
+  end
+
+  def has_commits commit_list
+    @commit_index < commit_list.size
   end
 
   def handle_if_match commit, track
     delta = diff_hours(commit[:timestamp], track[:timestamp])
     if delta >= 0 and delta < 2
-      @grouping << {
+      create_entry_if_needed track
+      @grouping[track[:music_id]][:count] += 1
+    else
+      return false
+    end
+    return true
+  end
+
+  def create_entry_if_needed track
+    if not @grouping.key? track[:music_id]
+      @grouping[track[:music_id]] = {
         music_id: track[:music_id],
-        count:1
+        count: 0,
       }
     end
   end
--- spec/mood_calculator_spec.rb
+++ spec/mood_calculator_spec.rb
@@ -23,7 +23,7 @@ describe MoodCalculator do
   it "empty data results in nothing" do
     expect(@calc.run [],[]).to eq []
     expect(@calc.run [],[commit(1,Time.now)]).to eq []
-    # expect(@calc.run [{music_id:1, timestamp:Time.now}],[]).to eq []
+    expect(@calc.run [track(1,Time.now)],[]).to eq []
   end
 
   it "returns all music count near 2 hours before a single commit" do
@@ -49,5 +49,43 @@ describe MoodCalculator do
                 ],
                 [ commit(1,stamp),commit(2,limit)]
     ).to eq [ count(1,1), count(2,1), count(3,1)]
+  end
+
+  it "does not count the same track twice" do
+    stamp = Time.new 2012,10,06, 13,33,45
+    midway = stamp - 1.hours
+    limit = stamp - 2.hours
+    expect(
+      @calc.run [ track(1,stamp),
+                  track(2,limit+1.minutes),
+                  track(3,limit-1.minutes),
+                ],
+                [ commit(1,stamp),commit(2,midway)]
+    ).to eq [ count(1,1), count(2,1), count(3,1)]
+  end
+
+  it "actually counts the music" do
+    stamp = Time.new 2012,10,06, 13,33,45
+    midway = stamp - 1.hours
+    limit = stamp - 2.hours
+    expect(
+      @calc.run [ track(1,stamp),track(1,stamp),track(1,stamp),
+                ],
+                [ commit(1,stamp)]
+    ).to eq [ count(1,3)]
+  end
+
+  it "order result by counting" do
+    stamp = Time.new 2012,10,06, 13,33,45
+    expect(
+      @calc.run [ track(2,stamp),
+                  track(1,stamp),
+                  track(3,stamp),
+                  track(1,stamp),
+                  track(2,stamp),
+                  track(1,stamp),
+                ],
+                [ commit(1,stamp)]
+    ).to eq [ count(1,3),count(2,2),count(3,1)]
   end
 end
