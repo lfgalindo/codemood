8e3a738158f422e5f2d2c51113e5d7468c01f151
Fabricio Buzeto <nuk.anime13@gmail.com>
2015-01-05 11:22:59 -0200

    Some refactoring

--- spec/mood_calculator_spec.rb
+++ spec/mood_calculator_spec.rb
@@ -6,6 +6,9 @@ describe MoodCalculator do
 
   before do
     @calc = MoodCalculator.new MoodUser.new
+    @stamp = Time.new 2012,10,06, 13,33,45
+    @midway = @stamp - 1.hours
+    @limit = @stamp - 2.hours
   end
 
   def track id, stamp, name = "Dummy Song / Dummy"
@@ -27,65 +30,54 @@ describe MoodCalculator do
   end
 
   it "returns all music count near 2 hours before a single commit" do
-    stamp = Time.new 2012,10,06, 13,33,45
-    limit = stamp - 2.hours
     expect(
       @calc.run [ 
-                  track(1,stamp),
-                  track(2,limit+1.minutes),
-                  track(3,limit-1.minutes),
+                  track(1,@stamp),
+                  track(2,@limit+1.minutes),
+                  track(3,@limit-1.minutes),
                 ],
-                [commit(1,stamp)]
+                [commit(1,@stamp)]
     ).to eq [count(1,1), count(2,1)]
   end
 
   it "returns all music count near 2 hours before all commits" do
-    stamp = Time.new 2012,10,06, 13,33,45
-    limit = stamp - 2.hours
     expect(
-      @calc.run [ track(1,stamp),
-                  track(2,limit+1.minutes),
-                  track(3,limit-1.minutes),
+      @calc.run [ track(1,@stamp),
+                  track(2,@limit+1.minutes),
+                  track(3,@limit-1.minutes),
                 ],
-                [ commit(1,stamp),commit(2,limit)]
+                [ commit(1,@stamp),commit(2,@limit)]
     ).to eq [ count(1,1), count(2,1), count(3,1)]
   end
 
   it "does not count the same track twice" do
-    stamp = Time.new 2012,10,06, 13,33,45
-    midway = stamp - 1.hours
-    limit = stamp - 2.hours
     expect(
-      @calc.run [ track(1,stamp),
-                  track(2,limit+1.minutes),
-                  track(3,limit-1.minutes),
+      @calc.run [ track(1,@stamp),
+                  track(2,@limit+1.minutes),
+                  track(3,@limit-1.minutes),
                 ],
-                [ commit(1,stamp),commit(2,midway)]
+                [ commit(1,@stamp),commit(2,@midway)]
     ).to eq [ count(1,1), count(2,1), count(3,1)]
   end
 
   it "actually counts the music" do
-    stamp = Time.new 2012,10,06, 13,33,45
-    midway = stamp - 1.hours
-    limit = stamp - 2.hours
     expect(
-      @calc.run [ track(1,stamp),track(1,stamp),track(1,stamp),
+      @calc.run [ track(1,@stamp),track(1,@stamp),track(1,@stamp),
                 ],
-                [ commit(1,stamp)]
+                [ commit(1,@stamp)]
     ).to eq [ count(1,3)]
   end
 
   it "order result by counting" do
-    stamp = Time.new 2012,10,06, 13,33,45
     expect(
-      @calc.run [ track(2,stamp),
-                  track(1,stamp),
-                  track(3,stamp),
-                  track(1,stamp),
-                  track(2,stamp),
-                  track(1,stamp),
+      @calc.run [ track(2,@stamp),
+                  track(1,@stamp),
+                  track(3,@stamp),
+                  track(1,@stamp),
+                  track(2,@stamp),
+                  track(1,@stamp),
                 ],
-                [ commit(1,stamp)]
+                [ commit(1,@stamp)]
     ).to eq [ count(1,3),count(2,2),count(3,1)]
   end
 end
